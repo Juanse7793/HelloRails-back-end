deb84142c5d555f0947a5e85b84b6b5a838f20bd
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-08-09 10:40:14 -0500

    API displaying random messages

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+Message.create(content: "Hello Tafara")
+Message.create(content: "Hello Juan")
+Message.create(content: "Hello Zaman")
+Message.create(content: "Hello Sebastian")
+Message.create(content: "Hello Pepe")
